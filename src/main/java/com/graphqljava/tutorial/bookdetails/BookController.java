2bcd17561a6ea17fb80122912cc5f81f96179b13
Erdal <erdal.guzel@ceng.deu.edu.tr>
2023-10-10 15:30:31 +0300

    Simple GraphQL application

---
+++ src/main/java/com/graphqljava/tutorial/bookdetails/BookController.java
@@ -0,0 +1,20 @@
+package com.graphqljava.tutorial.bookdetails;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getBookById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getAuthorById(book.getAuthorId());
+    }
+}
